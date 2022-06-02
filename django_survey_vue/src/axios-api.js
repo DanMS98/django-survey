f405c52410730b1fa1f9bfda0f1eb6f1200e8ab7
Danial <d.moradisabzevar@gmail.com>
2022-06-02 12:45:47 +0100

    addr fixed and prod build

--- django_survey_vue/src/axios-api.js
+++ django_survey_vue/src/axios-api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const getAPI = axios.create({
-    // baseURL: 'http://aris.ut.ac.ir/autsbackend',
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://aris.ut.ac.ir/autsbackend',
+    // baseURL: 'http://127.0.0.1:8000',
     timeout: 5000,
 })
 
